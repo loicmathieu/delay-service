d1cf20a5610029c5981efd3d4180d765aa96ce20
Loïc Mathieu <loikeseke@gmail.com>
2019-03-20 16:23:46 +0100

    feat: external config

--- src/main/java/fr/loicmathieu/delayservice/DelayServiceRest.java
+++ src/main/java/fr/loicmathieu/delayservice/DelayServiceRest.java
@@ -2,26 +2,29 @@ package fr.loicmathieu.delayservice;
 
 import java.util.concurrent.TimeUnit;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 @Path("/delay")
 public class DelayServiceRest {
-	private static final int DEFAULT_DELAY = 250;
+	@Inject @ConfigProperty(name="default.delay", defaultValue="250") private int defaultDelay;
+	@Inject @ConfigProperty(name="default.get.response", defaultValue="OK") private String getResponse;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String delay(@QueryParam("delay") Long delay) throws InterruptedException {
     	if(delay == null) {
-    		TimeUnit.MILLISECONDS.sleep(DEFAULT_DELAY);
+    		TimeUnit.MILLISECONDS.sleep(defaultDelay);
     	}
     	else {
     		TimeUnit.MILLISECONDS.sleep(delay);
     	}
     	
-        return "OK";
+        return getResponse;
     }
 }
